b2e759d6f7a9437a6920f99ec37ef8169f292023
Annalia <165366897+annalia12@users.noreply.github.com>
2024-05-31 14:44:18 +0200

    axios and api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+function refreshWeather(response){
+    let temperatureElement= document.querySelector("#temperature");
+    let temperature=response.data.temperature.current;
+    let cityElement= document.querySelector("#city");
+    cityElement.innerHTML=response.data.city;
+
+    temperatureElement.innerHTML= Math.round(temperature);
+}
+function searchCity(city){
+    let apiKey="eat3bf2c0ed301443o16635a4ff05de2";
+    let apiUrl=
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}"
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
-    let cityElement=document.querySelector("#city");
+    let cityElement=document.querySelector("#city"); 
     cityElement.innerHTML=searchInput.value;
+    searchCity(searchInput.value);
 }
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
